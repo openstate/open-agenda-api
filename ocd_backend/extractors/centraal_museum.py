463997d61091517535f84fafc6e77b03f8d2bbc6
Sicco van Sas <siccovansas@users.noreply.github.com>
2017-08-24 15:24:03 +0200

    Add Centraal Museum

---
+++ ocd_backend/extractors/centraal_museum.py
@@ -0,0 +1,37 @@
+from ocd_backend.extractors import BaseExtractor, HttpRequestMixin
+
+from lxml import etree
+
+
+class CentraalMuseumExtractor(BaseExtractor, HttpRequestMixin):
+    def get_collection_objects(self):
+        url = self.source_definition['url']
+
+        all_links_xpath = ".//ul[contains(@class,'itemList')]/li"
+
+        resp = self.http_session.get('%s/bezoeken/agenda/' % (url))
+        html = etree.HTML(resp.content)
+
+        # Loop over all div's containg links to item pages
+        items = html.xpath(all_links_xpath)
+
+        for item in items:
+            link = item.xpath("dl/dt/a/@href")
+            if link:
+                link = link[0]
+
+                # Add the base url if the link is a path (which is
+                # always the case afaik)
+                if not link.startswith('http'):
+                    link = url + link
+
+                yield link
+
+    def get_object(self, item_url):
+        resp = self.http_session.get(item_url, verify=False)
+
+        return 'application/html', resp.content
+
+    def run(self):
+        for item_url in self.get_collection_objects():
+            yield self.get_object(item_url)
