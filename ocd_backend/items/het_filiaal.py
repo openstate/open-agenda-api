dae249a7e3b055b81cd103432f27d742f5386701
Sicco van Sas <siccovansas@users.noreply.github.com>
2017-08-25 15:11:22 +0200

    Add Het Filiaal

---
+++ ocd_backend/items/het_filiaal.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+import re
+import urllib
+
+from ocd_backend.items import BaseItem
+
+
+class HetFiliaalItem(BaseItem):
+    def _get_text_or_none(self, xpath_expression):
+        node = self.original_item.find(xpath_expression)
+        if node is not None and node.text is not None:
+            return unicode(node.text)
+
+    def get_original_object_id(self):
+        # Use slug as object id; some slugs are longer than others so
+        # do some checking to find out what parts to use
+        slug = self.original_item.xpath(
+            './/meta[@property="og:url"]/@content'
+        )[0].split('/')[-2]
+
+        return unicode(slug)
+
+    def get_original_object_urls(self):
+        url = unicode(
+            self.original_item.xpath(
+                './/meta[@property="og:url"]/@content'
+            )[0]
+        )
+
+        return {
+            'html': url
+        }
+
+    def get_rights(self):
+        return u'Undefined'
+
+    def get_collection(self):
+        return u'Het Filiaal'
+
+    def get_combined_index_data(self):
+        combined_index_data = {
+            'hidden': self.source_definition['hidden']
+        }
+
+        return combined_index_data
+
+    def get_index_data(self):
+        return {}
+
+    def get_all_text(self):
+        text_items = []
+
+        return u' '.join(text_items)
